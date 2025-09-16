93cd370231d800eea43c95eff61eb79ffd7398ea
Matteo Moro <moro.matteorosario@gmail.com>
2025-09-17 01:13:40 +0200

    Inizio gamemenu.c

---
+++ gamemenu.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <raylib.h>
+#include <raygui.h>
+#include <string.h>
+
+#include "gamemenu-structs.h"
+//#include "blackjack-client.h"
+//non ancora riscritto quindi non esistente
+
+//sistemare i define, forse nel file da includere comune
+
+int StartMenu()
+{
+    struct GameMenu *menu = GameMenu_init();
+
+    if(menu != NULL)
+    {
+        BeginDrawing();
+
+        ClearBackground(DARKGREEN);
+
+        DrawTextEx(menu->titleFont, "Blackjack", menu->vecTitlePos, 128, 5, WHITE);
+        menu->playPressed = GuiButton(menu->ButtonArray[0], "PLAY");
+        menu->loginPressed = GuiButton(menu->ButtonArray[1], "LOGIN");
+        menu->registerPressed = GuiButton(menu->ButtonArray[2], "REGISTER");
+
+        if (menu->onlineMode == 1)
+        {
+            sprintf_s(menu->moneyText, TEXT_BUFF_SIZE, "MONEY = %d", menu->moneyValue);
+            DrawTextEx(menu->defaultFound, menu->moneyText, menu->vecMoneyTextPos, 64, 3, WHITE);
+        }
+
+    }
+
+    GameMenu_free(menu);
+    return -1;
+}
+
